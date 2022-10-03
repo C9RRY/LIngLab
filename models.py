16c195204d30f4d524c014be63fae40d138b69ea
C9RRY <ilya.lisovenko93@gmail.com>
2022-10-03 13:26:24 +0300

    optimise sql requests

--- models.py
+++ models.py
@@ -1,16 +1,17 @@
 import sqlite3
 
 
-database_path = 'ling_lab2.sqlite3'
+database_path = 'ling_lab.sqlite3'
 
 
 def create_user():
     conn = sqlite3.connect(database_path)
     cursor = conn.cursor()
     cursor.execute("CREATE TABLE users( "
-                   "id serial primary key NO NULL, "
+                   "id serial NOT NULL, "
                    "user_name varchar(50) UNIQUE, "
-                   "password varchar(50)")
+                   "password varchar(50), "
+                   "PRIMARY KEY (id))")
     conn.commit()
 
 
@@ -18,10 +19,10 @@ def create_dictionary():
     conn = sqlite3.connect(database_path)
     cursor = conn.cursor()
     cursor.execute("CREATE TABLE dictionary( "
-                   "id serial primary key NO NULL, "
-                   "word varchar(250), "
-                   "translation varchar(250) NO NULL UNIQUE, "
-                   "pronounce filefield ")
+                   "id serial PRIMARY KEY NOT NULL, "
+                   "word VARCHAR(250), "
+                   "translation VARCHAR(250) NOT NULL UNIQUE, "
+                   "pronounce FILEFIELD) ")
     conn.commit()
 
 
@@ -29,7 +30,7 @@ def create_vocabulary():
     conn = sqlite3.connect(database_path)
     cursor = conn.cursor()
     cursor.execute("CREATE TABLE vocabulary( "
-                   "id serial primary key NO NULL, "
+                   "id serial PRIMARY KEY NOT NULL, "
                    "user_id INTEGER, "
                    "word_id INTEGER, "
                    "progress VARCHAR(10) DEFAULT '*', "
@@ -38,10 +39,11 @@ def create_vocabulary():
                    "progress_audition BOOLEAN, "
                    "progress_repeat BOOLEAN, "
                    "FOREIGN KEY('user_id') REFERENCES 'users'('id'), "
-                   "FOREIGN KEY('word_id') REFERENCES 'dictionary'('id') ")
+                   "FOREIGN KEY('word_id') REFERENCES 'dictionary'('id') ) ")
     conn.commit()
 
 
 if __name__ == "__main__":
-    pass
-
+    create_user()
+    create_dictionary()
+    create_vocabulary()
